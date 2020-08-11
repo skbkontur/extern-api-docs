280806862a18b51c3273c6fc4717bf220c9fb8ce
buravova <buravova@skbkontur.ru>
2020-08-11 15:23:37 +0500

    ext in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,8 +33,8 @@ compat.make_admonition = BaseAdmonition
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = ['sphinx.ext.autodoc', 'hoverxref.extension', 'sphinx_tabs.tabs']
-
+#extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.httpdomain', 'sphinxcontrib.images', 'hoverxref.extension']
+extensions = ['sphinx_tabs.tabs']
 images_config = {
     'override_image_directive': True
 }
